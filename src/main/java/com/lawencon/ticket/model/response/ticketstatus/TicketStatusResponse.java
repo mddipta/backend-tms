a4993d81bae90b25a340e4f2b2eaa3d84bae0aca
dhanzydev <200030281@stikom-bali.ac.id>
2024-11-24 14:22:56 +0700

    edit: status ticket response

--- src/main/java/com/lawencon/ticket/model/response/ticketstatus/TicketStatusResponse.java
+++ src/main/java/com/lawencon/ticket/model/response/ticketstatus/TicketStatusResponse.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TicketStatusResponse {
-    private Long id;
+    private String id;
     private String code;
     private String name;
     private Long version;
